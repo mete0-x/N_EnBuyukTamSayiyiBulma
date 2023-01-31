da5fa7caf0cfa90728b365856285f139882cd4e4
mete0-x <meteturgutttt@gmail.com>
2023-01-31 21:10:00 +0300

    N_EnBuyukTamSayiyiBulma
    N_EnBuyukTamSayiyiBulma

---
+++ src/main/java/n_sayi.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class n_sayi {
+    public static void main(String[] args){
+      int n= 0;
+      
+        while (Math.pow(n +1, 3) < 12000) {
+            n++;
+            
+        }
+
+        System.out.println("n  :"+n);
+    }
+}
